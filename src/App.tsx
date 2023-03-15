7f0d227599126f0a646b14b5a4883df0107aedc2
Kenny Baron <kennybaron@fastmail.com>
2023-03-15 15:28:51 -0400

    wip results item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
+// import savedResults from './results.json' assert {type: 'json'}
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [searchText, setSearchText] = useState('');
+  const [results, setResults] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchJsonFile('http://localhost:5173/src/results.json');
+      console.log(`data`, data.docs[0].title)
+      
+      setResults(data);
+    };
+    fetchData();
+  }, []);  
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleSearchSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!searchText) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axios.get(`https://openlibrary.org/search.json?q=${searchText}`);
+      setResults(response.data);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} but bount is bount
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search for books"
+          value={searchText}
+          onChange={handleSearchInputChange}
+        />
+        <button type="submit">
+          🔍
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        results && (
+          <div className="results">
+            <p>Found {results.numFound} results</p>
+            <ul>
+              {
+                results.docs.map((doc: any, i: number) => (
+                  <ResultsItem doc={doc} key={i}/>
+                ))
+              }
+            </ul>
+          </div>
+        )
+      )}
     </div>
+  );
+};
+
+export default App
+
+interface IResultsItem {
+  doc: any;
+  key: number;
+}
+const ResultsItem: React.FC<IResultsItem> = ({ doc, key }) => {
+  return (
+    <ul key={key}>
+      <h2>{doc.title}</h2>
+      <li>author: {doc.author_name}</li>
+      <li>published on: {doc.publish_date}</li>
+      <li>publisher: {doc.publisher}</li>
+      <li>1st isbn: {doc.isbn?.[0]}</li>
+    </ul>
+
   )
 }
 
-export default App
+const Spinner: React.FC = () => {
+  return <div className="spinner"></div>
+}
+
+async function fetchJsonFile(fileUrl: string) {
+  try {
+    const response = await fetch(fileUrl);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const jsonObject = await response.json();
+    return jsonObject;
+  } catch (error) {
+    console.error('Error fetching JSON file:', error);
+    return null;
+  }
+}
